3b30bec2ac08d709235d7f351101112f17c492af
amidsts <mustaphaamidatoyindamola@gmail.com>
2025-06-13 08:34:02 +0100

    module import update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * as middlewares from './middlewares/require-auth';
+export * as middlewares from './middlewares';
 
 export * as errors from './utils/errors';
-export * as logger from './utils/logger';
-export * as utils from './utils/types';
+export * from './utils/logger';
+export * from './utils/types';
 
-export * as rmq from './amqplib/src';
+export * from './amqplib/src';
